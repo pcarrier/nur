60c41fc2020c4a93f4d6869568ae782425e56b36
Pierre Carrier <pc@rrier.fr>
2023-05-16 15:41:23 +0200

    hmmm

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,18 @@
-use std::{env, io};
+use std::env;
 use std::error::Error;
 use std::io::Read;
 
-use http_cache_reqwest::{CACacheManager, Cache, CacheMode, HttpCache};
-use reqwest::Client;
-use reqwest_middleware::ClientBuilder;
-use rquickjs::{Context, Func, Runtime};
 use url::{ParseError, Url};
+
+use crate::nur::Nur;
+
+mod nur;
 
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
-fn print(msg: String) {
-    println!("{msg}");
-}
 
-fn full_stdin() -> String {
-    let mut str = String::new();
-    io::stdin().read_to_string(&mut str).unwrap();
-    str
-}
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let url = env::args().nth(1).and_then(|s| derive_url(s).ok())
-        .unwrap_or(Url::parse("https://nur.tools/badurl").unwrap());
-
-    let client = ClientBuilder::new(Client::new())
-        .with(Cache(HttpCache {
-            mode: CacheMode::Default,
-            manager: CACacheManager {
-                path: dirs::cache_dir()
-                    .and_then(|p|
-                        p.join("tools.nur")
-                            .into_os_string().into_string().ok())
-                    .unwrap_or_else(|| ".tools.nur/cache".to_string())
-            },
-            options: None,
-        }))
-        .build();
-
-    let body = client.get(url.clone())
-        .header("User-Agent", "nur")
-        .send()
-        .await?
-        .text()
-        .await?;
-
-    let rt = Runtime::new()?;
-    let ctx = Context::full(&rt)?;
-
-    ctx.with(|ctx| -> Result<(), Box<dyn Error>> {
-        let globals = ctx.globals();
-        globals.set("print", Func::new("print", print))?;
-        globals.set("full_stdin", Func::new("full_stdin", full_stdin))?;
-        ctx.compile(url.to_string(), body)?;
-        Ok(())
-    })
-}
-
-fn derive_url(short: String) -> Result<Url, ParseError> {
+fn cli_url(short: String) -> Result<Url, ParseError> {
     if short.starts_with("@") {
         return Url::parse("https://nur.tools")?.join(&short[1..]);
     }
@@ -67,4 +20,11 @@ fn derive_url(short: String) -> Result<Url, ParseError> {
         return Url::parse(&short);
     }
     Url::parse(format!("https://{short}").as_str())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let url = env::args().nth(1).and_then(|s| cli_url(s).ok())
+        .unwrap_or(Url::parse("https://nur.tools/badurl").unwrap());
+    Nur::new(url)?.run().await
 }
---
+++ src/nur.rs
@@ -0,0 +1,89 @@
+use io::{stderr, stdin, stdout};
+use std::error::Error;
+use std::io;
+use std::io::{Read, Stderr, Stdin, Stdout};
+use http_cache_reqwest::{CACacheManager, Cache, CacheMode, HttpCache};
+use reqwest::{Client, StatusCode};
+use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
+use rquickjs::{Context, Func, Runtime};
+use url::Url;
+
+pub(crate) struct Nur {
+    url: Url,
+    rt: Runtime,
+    client: ClientWithMiddleware,
+    stdin: Stdin,
+    stdout: Stdout,
+    stderr: Stderr,
+}
+
+impl Nur {
+    pub(crate) fn new(url: Url) -> Result<Self, Box<dyn Error>> {
+        let client = ClientBuilder::new(Client::new())
+            .with(Cache(HttpCache {
+                mode: CacheMode::Default,
+                manager: CACacheManager {
+                    path: dirs::cache_dir()
+                        .and_then(|p|
+                            p.join("tools.nur")
+                                .into_os_string().into_string().ok())
+                        .unwrap_or_else(|| ".tools.nur/cache".to_string())
+                },
+                options: None,
+            }))
+            .build();
+
+        let rt = Runtime::new()?;
+
+        Ok(Self {
+            url,
+            rt,
+            client,
+            stdin: stdin(),
+            stdout: stdout(),
+            stderr: stderr(),
+        })
+    }
+
+    async fn fetch(&self, url: Url) -> Result<String, Box<dyn Error>> {
+        let res = self.client.get(url)
+            .send()
+            .await?;
+        if res.status() != StatusCode::OK {
+            return Err(format!("fetch failed: {}", res.status()).into());
+        }
+        Ok(res.text().await?)
+    }
+
+    async fn fetch_js(&self, url: Url) -> Result<String, Box<dyn Error>> {
+        if url.scheme() == "file" {
+            return Ok(std::fs::read_to_string(url.path())?);
+        }
+
+        let mut decorated = url.clone();
+        decorated.query_pairs_mut().append_pair("nur-get", "1");
+        self.fetch(decorated).await
+    }
+
+    fn print(&self, msg: String) {
+        println!("{msg}");
+    }
+
+    fn full_stdin(mut self) -> String {
+        let mut str = String::new();
+        self.stdin.read_to_string(&mut str).unwrap();
+        str
+    }
+
+    pub(crate) async fn run(&self) -> Result<(), Box<dyn Error>> {
+        let code = self.fetch_js(self.url.clone()).await?;
+        let ctx = Context::full(&self.rt)?;
+        ctx.with(move |ctx| -> Result<(), Box<dyn Error>> {
+            let globals = ctx.globals();
+            globals.set("print", Func::new("print", |v| self.print(v)))?;
+            globals.set("full_stdin", Func::new("full_stdin", || self.full_stdin()))?;
+            ctx.compile(self.url.to_string(), code)?;
+            Ok(())
+        })
+    }
+}
